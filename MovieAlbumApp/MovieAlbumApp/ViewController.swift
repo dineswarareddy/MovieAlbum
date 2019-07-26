8787274bbfa29fa579415f46456479464c6e53b8
Dinesh Reddy Chennuru <dineswarareddy@gmail.com>
2019-07-26 19:49:40 +0530

    :gift: Setup Application.

---
+++ MovieAlbumApp/MovieAlbumApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieAlbumApp
+//
+//  Created by Dinesh Reddy on 26/07/19.
+//  Copyright © 2019 Dinesh Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
