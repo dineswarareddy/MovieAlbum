0b7e02a69a06386d976d58202a5da946f413291b
Dinesh Reddy Chennuru <dineswarareddy@gmail.com>
2019-07-26 21:19:06 +0530

    :arrow_up: Done Setup VIPER architecture for Movie Details Screen.

---
+++ MovieAlbumApp/MovieAlbumApp/MovieDetails/UserInterface/MovieDetailsPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDetailsPresenter.swift
+//  MovieAlbumApp
+//
+//  Created by Dinesh Reddy on 26/07/19.
+//  Copyright © 2019 Dinesh Reddy. All rights reserved.
+//
+
+import Foundation
